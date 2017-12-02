6835d01ca289589fcc1b804df5b19f24b06d280b
Daniel Justus <daniel.justus32@gmail.com>
2017-12-01 21:57:56 -0600

    Begin writing tests for service class

--- lib/stl_public_services.rb
+++ lib/stl_public_services.rb
@@ -3,5 +3,7 @@ require_relative "stl_public_services/cli_controller"
 require "./lib/stl_public_services/scraper"
 require "./lib/stl_public_services/service"
 
+require 'pry'
+
 module StlPublicServices
 end
---
+++ spec/service_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe StlPublicServices::Service do
+	let(:service) { StlPublicServices::Service.new("Affordable Housing Commission", "(314) 657-3880", "(314) 613-7015", "1520 Market Street", "https://www.stlouis-mo.gov/government/departments/affordable-housing/index.cfm") }
+end
