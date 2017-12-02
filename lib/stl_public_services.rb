cdc5d7e4e61c4062e793ef6ce994bc66a1108e55
Daniel Justus <daniel.justus32@gmail.com>
2017-12-01 21:31:33 -0600

    Move environment to lib/stl_public_services, since it was already created for that purpose

--- lib/stl_public_services.rb
+++ lib/stl_public_services.rb
@@ -1,4 +1,7 @@
-require "stl_public_services/version"
+require_relative "stl_public_services/version"
+require_relative "stl_public_services/cli_controller"
+require "./lib/stl_public_services/scraper"
+require "./lib/stl_public_services/service"
 
 module StlPublicServices
 end
--- lib/stl_public_services/cli_controller.rb
+++ lib/stl_public_services/cli_controller.rb
@@ -9,11 +9,12 @@ class StlPublicServices::CliController
 	end
 
 	def welcome_user
-		<<-WELCOME.gsub /^\s+/, ""
+		puts <<-WELCOME.gsub /^\s+/, ""
 			Welcome to the St. Louis Public Services Ruby 
 			CLI Gem! To list all available information on 
 			St. Louis City Public Services and Government 
 			Departments, type "list". You can also type 
 			"exit".
 		WELCOME
+	end
 end
