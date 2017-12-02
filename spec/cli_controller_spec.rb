92200d32daa93e4716e66a859db18ae9c7c39741
Daniel Justus <daniel.justus32@gmail.com>
2017-12-01 20:48:35 -0600

    Update cli_controller_spec to include method names in descriptions

--- spec/cli_controller_spec.rb
+++ spec/cli_controller_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 RSpec.describe StlPublicServices::CliController do 
 	let(:controller) { StlPublicServices::CliController.new}
 
-	it "starts the command-line interface" do 
-		controller.call
-		expect(controller.start).to have_been_called
+	describe "#call" do
+		it "starts the command-line interface" do 
+			controller.call
+			expect(controller.start).to have_been_called
+		end
 	end
 end
