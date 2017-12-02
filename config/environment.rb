ccf212203c195dc5d9b3303faab59de2db7a43f3
Daniel Justus <daniel.justus32@gmail.com>
2017-12-01 22:06:46 -0600

    Continue writing tests for service class

--- config/environment.rb
+++
@@ -1,3 +0,0 @@
-require "./lib/stl_public_services/cli_controller"
-require "./lib/stl_public_services/scraper"
-require "./lib/stl_public_services/service"
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
+require 'pry'
 
 RSpec.describe StlPublicServices::Service do
 	let(:service) { StlPublicServices::Service.new("Affordable Housing Commission", "(314) 657-3880", "(314) 613-7015", "1520 Market Street", "https://www.stlouis-mo.gov/government/departments/affordable-housing/index.cfm") }
+
+	describe "#initialize" do
+		it "creates an instance of a service with all contact info" do
+			expect(service.instance_variable_get(:@name)).to eq("Affordable Housing Commission")
+			service.instance_variable_get(:@name)
+		end 
+	end
 end
