fa1ca25d60ba854ea101ca488cbf88a7d04e6369
Daniel Justus <daniel.justus32@gmail.com>
2017-12-01 21:13:53 -0600

    Update Cli Class to include several methods to test that spec is working.

--- lib/stl_public_services/cli_controller.rb
+++ lib/stl_public_services/cli_controller.rb
@@ -1,3 +1,19 @@
 class StlPublicServices::CliController
 
+	def call
+		start
+	end
+
+	def start
+		welcome_user
+	end
+
+	def welcome_user
+		<<-WELCOME.gsub /^\s+/, ""
+			Welcome to the St. Louis Public Services Ruby 
+			CLI Gem! To list all available information on 
+			St. Louis City Public Services and Government 
+			Departments, type "list". You can also type 
+			"exit".
+		WELCOME
 end
