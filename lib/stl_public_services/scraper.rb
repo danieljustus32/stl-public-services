ba491a0deb6a4ea1972a891ca3796ba771ca741f
Daniel Justus <daniel.justus32@gmail.com>
2017-12-04 20:21:40 -0600

    Refactor scraper class

--- lib/stl_public_services/scraper.rb
+++ lib/stl_public_services/scraper.rb
@@ -11,10 +11,16 @@ class StlPublicServices::Scraper
 
 	def scrape_service_info(url)
 		doc = Nokogiri::HTML(open(url))
-		a = doc.css('h4 a').map do |listing|
+		name = nil	
+		phone = nil	
+		fax = nil
+		address = nil	
+		url = nil
+		doc.css('h4 a').map do |listing|
 			name = listing.text
-			url = listing['href']
+			url = listing ['href']	
 		end
+		a = doc.css
 		binding.pry
 	end
 end
