fa2c6309f2a5349155888ad15c539a24bce1054c
Daniel Justus <daniel.justus32@gmail.com>
2017-12-05 21:50:58 -0600

    Refactor #initialize test

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe StlPublicServices::Service do
 	describe "#initialize" do
 		it "creates an instance of a service with all contact info" do
 			expect(service.instance_variable_get(:@name)).to eq("Affordable Housing Commission")
+			expect(service.instance_variable_get(:@url)).to eq("https://www.stlouis-mo.gov/government/departments/affordable-housing/index.cfm")
+			expect(service.instance_variable_get(:@phone)).to eq("(314) 657-3880")
+			expect(service.instance_variable_get(:@fax)).to eq("(314) 613-7015")
+			expect(service.instance_variable_get(:@address)).to eq("1520 Market Street")
 			service.instance_variable_get(:@name)
+			service.instance_variable_get(:@url)
+			service.instance_variable_get(:@phone)
+			service.instance_variable_get(:@fax)
+			service.instance_variable_get(:@address)
 		end 
 	end
 
@@ -23,6 +31,12 @@ RSpec.describe StlPublicServices::Service do
 			service.save
 			expect(service.class.all.size).not_to eq(0)
 			service.class.all.size
+		end
+	end
+
+	describe "#find_or_create_by_name" do
+		it "finds a service in @@all or creates a new one" do
+	
 		end
 	end
 end
